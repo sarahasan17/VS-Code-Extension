2d4f750f7fe1b5e34ccb4c2f69325910844dab5b
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-06-09 10:57:50 +0530

    web page url search add on

--- extension.js
+++ extension.js
@@ -1,34 +1,102 @@
-
 const vscode = require('vscode');
-const axios =require('axios');
-const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
-const parser = new XMLParser();
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-async function activate(context) {
-	const res = await axios.get('https://blog.webdevsimplified.com/rss.xml');
-	const m=parser.parse(res.data).rss.channel.item.map(article=> {return {label:article.title,
-		detail:article.description,
-		link:article.link
-	}});
-	console.log(m);
-	console.log('Congratulations, your extension "search-blogs-" is now active!');
-	const disposable = vscode.commands.registerCommand('search-blogs-.Alhamdulillah', 
-		async function () {
-		const article=await vscode.window.showQuickPick(m,{
-			matchOnDetail:true,
-		})
-		if (article==null)return;
-		vscode.env.openExternal(article.link);
-		})
-	context.subscriptions.push(disposable);
+function activate(context) {
+    console.log('Congratulations, your extension "search-blogs" is now active!');
+
+    // Register the command to open the webview
+    let disposable = vscode.commands.registerCommand('search-blogs.openSearch', async function () {
+        const panel = vscode.window.createWebviewPanel(
+            'frontend', // Identifies the type of the webview. Used internally
+            'Display Webpage', // Title of the panel displayed to the user
+            vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+            {
+                enableScripts: true // Enable JavaScript in the webview
+            }
+        );
+
+        // Get the path to the HTML file
+        const htmlPath = vscode.Uri.file(path.join(context.extensionPath, 'frontend', 'index.html'));
+        const htmlContent = fs.readFileSync(htmlPath.fsPath, 'utf8');
+
+        // Set the HTML content
+        panel.webview.html = getWebviewContent(htmlContent);
+
+        // Handle messages from the webview
+        panel.webview.onDidReceiveMessage(
+            async message => {
+                switch (message.command) {
+                    case 'search':
+                        const url = message.url;
+                        try {
+                            const response = await axios.get(url);
+                            const webpageContent = response.data;
+                            panel.webview.postMessage({ command: 'displayWebpage', content: webpageContent });
+                        } catch (error) {
+                            panel.webview.postMessage({ command: 'displayWebpage', content: `<p>Error loading webpage: ${error.message}</p>` });
+                        }
+                        return;
+                }
+            },
+            undefined,
+            context.subscriptions
+        );
+    });
+
+    context.subscriptions.push(disposable);
+
+    // Automatically open the webview when the extension is activated
+    vscode.commands.executeCommand('search-blogs.openSearch');
+}
+
+function getWebviewContent(htmlContent) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Display Webpage</title>
+    </head>
+    <body>
+        ${htmlContent}
+        <script>
+            const vscode = acquireVsCodeApi();
+
+            document.getElementById('searchForm').addEventListener('submit', event => {
+                event.preventDefault();
+                const url = document.getElementById('searchInput').value;
+                vscode.postMessage({ command: 'search', url: url });
+            });
+
+            window.addEventListener('message', event => {
+                const message = event.data;
+                switch (message.command) {
+                    case 'displayWebpage':
+                        displayWebpage(message.content);
+                        break;
+                }
+            });
+
+            function displayWebpage(content) {
+                const results = document.getElementById('results');
+                results.innerHTML = content;
+            }
+        </script>
+    </body>
+    </html>
+    `;
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
